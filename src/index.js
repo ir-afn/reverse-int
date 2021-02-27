a78f6d28deb91341a3007ee69cd8a50e2a256c1f
ir-afn <46682788+ir-afn@users.noreply.github.com>
2021-02-27 21:25:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let num = Math.abs(n);
+  let str = num.toString();
+  let result = '';
+
+  if (Math.floor(num / 10) === num / 10) {
+    for (let i = str.length - 2; i >= 0; i -= 1) {
+      result += str[i];
+    }
+  } else {
+    for (let i = str.length - 1; i >= 0; i -= 1) {
+      result += str[i];
+    }
+  }
+  return result * 1;
 }
